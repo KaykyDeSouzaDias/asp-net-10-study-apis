17918eb757011244faafb17f485013215d26f517
Kayky Dias <kayky.dias@brilliantmachine.com.br>
2026-01-10 16:01:52 -0300

    Feat: Hello world & simple sum calculation

---
+++ ApiStudy/ApiStudy/Controllers/CalculatorController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ApiStudy.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CalculatorController : Controller
+    {
+        [HttpGet("sum/{firstNum}/{secondNum}")]
+        public IActionResult Get(string firstNum, string secondNum)
+        {
+            if (IsNumeric(firstNum) && IsNumeric(secondNum))
+            {
+                var sum = ConvertToDecimal(firstNum) + ConvertToDecimal(secondNum);
+                return Ok(sum);
+            }
+            return BadRequest("Invalid input");
+        }
+
+        private decimal ConvertToDecimal(string value)
+        {
+            decimal decimalValue;
+            if (decimal.TryParse(value,
+                System.Globalization.NumberStyles.Any,
+                System.Globalization.NumberFormatInfo.InvariantInfo,
+                out decimalValue))
+            {
+                return decimalValue;
+            }
+            return 0;
+        }
+
+        private bool IsNumeric(string value)
+        {
+            decimal decimalValue;
+            bool isNumber = decimal.TryParse(value,
+                System.Globalization.NumberStyles.Any,
+                System.Globalization.NumberFormatInfo.InvariantInfo,
+                out decimalValue);
+            return isNumber;
+        }
+    }
+}
